da3f6d50ba42f3fbd5d3c2cee09a5f2d40afa3fb
isidordebout <isidordebout71@gmail.com>
2014-08-15 16:42:41 +0200

    Update ParentProcess.cs

--- ParentProcess.cs
+++ ParentProcess.cs
@@ -11,42 +11,58 @@ namespace Parent
 {
     class Program
     {
+        private static TResult Spawn<TParam, TResult>(string path, TParam param, int timeout)
+        {
+            var srz = new BinaryFormatter();
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.FileName = path;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = true;
+            try
+            {
+                p.Start();
+
+                srz.Serialize(p.StandardInput.BaseStream, param);
+                p.StandardInput.Flush();
+
+                if (!p.WaitForExit(timeout))
+                    throw new TimeoutException();
+                p.StandardOutput.DiscardBufferedData();// Se positionne au début du flux
+                var result =  (TResult)srz.Deserialize(p.StandardOutput.BaseStream);
+                p.Close();
+                return result;
+            }
+            catch
+            {
+                if (!p.HasExited)
+                {
+                    try { p.Kill(); }
+                    catch { }
+                }
+                throw;
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.ReadKey();
             var sw = Stopwatch.StartNew();
+            const string path = "C:\\Users\\Toto\\Desktop\\Processes\\Child\\Child\\bin\\Release\\Child.exe";
             for (int i = 0; i < 100; i++)
             {
                 try
                 {
-                    Process myProcess = new Process();
-
                     var dico = new Dictionary<string, string>()
-                {
-                    {"key1", "value1"},
-                    {"key2", "value2"},
-                    {"key3", "value3"}
-                };
+                    {
+                        {"key1", "value1"},
+                        {"key2", "value2"},
+                        {"key3", "value3"}
+                    };
 
-                    var srz = new BinaryFormatter();
-
-                    myProcess.StartInfo.UseShellExecute = false;
-                    // You can start any process, HelloWorld is a do-nothing example.
-                    myProcess.StartInfo.FileName = "C:\\Users\\Toto\\Desktop\\Processes\\Child\\Child\\bin\\Release\\Child.exe";
-                    myProcess.StartInfo.CreateNoWindow = true;
-                    myProcess.StartInfo.RedirectStandardOutput = true;
-                    myProcess.StartInfo.RedirectStandardInput = true;
-                    myProcess.Start();
-
-                    // This code assumes the process you are starting will terminate itself.  
-                    // Given that is is started without a window so you cannot terminate it  
-                    // on the desktop, it must terminate itself or you can do it programmatically 
-                    // from this application using the Kill method.
-                    srz.Serialize(myProcess.StandardInput.BaseStream, dico);
-                    myProcess.StandardInput.Flush();
-                    myProcess.StandardOutput.ReadToEnd();
-                    myProcess.WaitForExit();
-                    myProcess.Close();
+                    var result = Spawn<Dictionary<string, string>, double>(path, dico, 200);
+                    //Console.WriteLine(result);
                 }
                 catch (Exception e)
                 {
